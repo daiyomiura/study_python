dae7a33f50421a1cd7ddbacc9c9311c455b3bd78
Daiyo Miura <d-miura@clinks.jp>
2019-03-22 13:27:00 +0900

    [tutorial]～5.4

--- editor/tutorial/tutorial.py
+++ editor/tutorial/tutorial.py
@@ -192,3 +192,33 @@ for i in range(4):
 transposed
 
 list(zip(*matrix))
+
+# 5.2. The del statement
+a = [-1, 1, 66.25, 333, 333, 1234.5]
+del a[0]
+a
+del a[2:4]
+a
+del a[:]
+a
+del a # delete entire variables
+
+# 5.3. Tuples and Sequences
+t = 12345, 54321, 'hello!'
+t[0]
+t
+# Tuples may be nestd:
+u = t, (1,2,3,4,5)
+u
+# Tuples are immutable:
+t[0] = 88888;
+# but they can contain mutable objects:
+v = ([1, 2, 3], [3, 2, 1])
+v
+
+empty = ()
+singleton = 'hello', # <-- note trailing comma
+len(empty)
+len(singleton)
+singleton
+x, y, z = t
