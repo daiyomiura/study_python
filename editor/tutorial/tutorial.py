5ff81fd8950be055751899d174a95832764c0805
Daiyo Miura <d-miura@clinks.jp>
2019-04-09 16:08:29 +0900

    ～7.1.2

--- editor/tutorial/tutorial.py
+++ editor/tutorial/tutorial.py
@@ -377,3 +377,32 @@ hellos = repr(hello)
 print(hellos)
 # The argument to repr() may any Python object:
 repr((x, y, ('spam', 'eggs')))
+
+# 7.1.1. FOrmatted String Literals
+import math
+print(f'The value of pi is approximatery {math.pi:.3f}.')
+
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 7678}
+for name, phone in table.items():
+    print(f'{name:10} ==> {phone:10d}')
+animals = 'eels'
+print(f'My hovercraft is full of {animals}.')
+print(f'My hovercraft is full of {animals!r}.')
+
+7.1.2. The String format() method
+print('We are the {} who say "{}!"'.format('knights', 'Ni'))
+
+print('{0} and {1}'.format('spam', 'eggs'))
+print('{1} and {0}'.format('spam', 'eggs'))
+
+print('This {food} is {adjective}'.format(
+    food='spam', adjective='absolutely horrible'))
+
+print('The story of {0}, {1}, and {other}.'.format('Bill', 'Manfred', other='Georg'))
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 8637678}
+print('Jack: {0[Jack]:d}; Sjoerd: {0[Sjoerd]:d}; Dcab: {0[Dcab]:d}'.format(table))
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 8637678}
+print('Jack: {Jack:d}; Sjoerd: {Sjoerd:d}; Dcab: {Dcab:d}'.format(**table))
+
+for x in range(1, 11):
+    print('{0:2d} {1:3d} {2:4d}'.format(x, x*x, x*x*x))
