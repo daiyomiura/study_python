6697c7f113a2208db4a22ddf476c9338be9000fc
Daiyo Miura <d-miura@clinks.jp>
2019-04-19 18:34:00 +0900

    Python Tutorial
    ～10.12

--- editor/tutorial/tutorial.py
+++ editor/tutorial/tutorial.py
@@ -811,3 +811,107 @@ dir(os)
 help(os)
 import shutil
 shutil.copyfile('data.db', 'archive.db')
+
+# 10.2. File Wildcards
+import glob
+glob.glob("*.py")
+
+# 10.3. Command Line Arguments
+import sys
+print(sys.argv)
+# 10.4. Error Output Redirection and Program Termination
+sys.stderr.write('Warning, log file not found starting a new one\n')
+# 10.5. String Pattern Matching
+import re
+re.findall(r'\bf[a-z]*', 'which foot or hand fell fastest')
+re.sub(r'(b[a-z]+) \1', r'\1', 'cat in the the hat')
+
+'tea for too'.replace('too', 'two')
+
+# 10.6. Mathematics
+import math
+math.cos(math.pi / 4)
+math.log(1024, 2)
+
+import random
+random.choice(['apple', 'pear', 'banana'])
+random.sample(range(100), 10)   # sampling without replacement
+random.random()
+random.randrange(6) # random integer chosen from range(6)
+
+
+import statistics
+data = [2.75, 1.75, 1.25, 0.25, 0.5, 1.25, 3.5]
+statistics.mean(data)
+statistics.median(data)
+statistics.variance(data)
+
+# 10.7. Internet Access
+from urllib.request import urlopen
+with urlopen('http://tycho.usno.navy.mil/cgi-bin/timer.pl') as response:
+    for line in response:
+        line = line.decode('utf-8') # Decoding the binary data to text
+        if 'EST' in line or 'EDT' in line:  # look for Eastern Time
+            print(line)
+
+import smtplib
+server = smtplib.SMTP('localhost')
+
+
+# 10.8. Dates and Times
+# dates are easily constructed and formatted
+from datetime import date
+now = date.today()
+now
+
+now.strftime('%m-%d-%y. %d %b %Y is a %A on the %d day of %B.')
+# dates support calendar arithmetic
+birthday = date(1973, 12, 5)
+age = now - birthday
+age.days
+
+# 10.9. Data Compression
+import zlib
+s = b'witch which has which witches wrist watch'
+len(s)
+
+t = zlib.compress(s)
+len(t)
+
+zlib.decompress(t)
+
+zlib.crc32(s)
+
+# 10.10. Performance Measurement
+from timeit import Timer
+Timer('t=a; a=b; b=t', 'a=1; b=2').timeit()
+
+Timer('a,b = b,a', 'a=1; b=2').timeit()
+
+# 10.11 Quality Control
+def average(values):
+    """Computes the arithmetic mean of a list of numbers.
+
+    >>> print(average([20, 30, 70]))
+    40.0
+    """
+    return sum(values) / len(values)
+
+import doctest
+doctest.testmod()   # automatically validate the embedded tests
+
+import unittest
+
+class TestStatisticalFUnctions(unittest.TestCase):
+
+    def test_average(self):
+        self.assertEqual(average([20, 30, 70]), 40.0)
+        self.assertEqual(round(average([1, 5, 7]), 1), 4.3)
+        with self.assertRaises(ZeroDivisionError):
+            average([])
+        with self.assertRaises(TypeError):
+            average(20, 30, 70)
+
+unittest.main() # Calling from the comand line invokes all tests
+
+# 10.12 Batteries included
