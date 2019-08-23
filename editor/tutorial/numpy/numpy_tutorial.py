bf9d19118c151b1182386f39024be4360c2a184f
Daiyo Miura <d-miura@clinks.jp>
2019-08-23 09:59:59 +0900

    少し進めた

--- editor/tutorial/numpy/numpy_tutorial.py
+++ editor/tutorial/numpy/numpy_tutorial.py
@@ -31,3 +31,28 @@ hw = hello + ' ' + world    # String concatenation
 print(hw)   # prints "hello world"
 hw12 = '%s %s %d' % (hello, world, 12)  # sprintf style string formating.
 print(hw12) # prints "hello world 12"
+
+# String objects have a bunch of useful methods; for example:
+s = "hello"
+print(s.capitalize())   # Capitalize a string; prints "Hello"
+print(s.upper())        # Convert a string to uppercase; prints "HELLO"
+print(s.rjust(7))       # Right-justify a string, padding with spaces; prints "
+print(s.center(7))      # Center a string, padding with spaces; prints " hello "
+print(s.replace('l', '(ell)'))  # Replace all instances of one substring with another;
+                                # prints(he(ell)(ell)o)
+print('   world'.strip())       # Strip leading and trailing whitespace; prints "world"
+# Containers
+# Python includs several built-in container types: lists, dictionaries, sets, and tuples.
+
+# Lists
+#  A list is the Python equivalent of an array, but is resizeable and can contain elements of different types:
+
+xs = [3, 1, 2]      # Create list
+print(xs, xs[2])    # Prints "[3, 1, 2 ] 2"
+print(xs[-1])       # Negative indices count from the end of the list; prints "2"
+xs[2] = 'foo'       # Lists can contain elements of different types
+print(xs)           # Prints "[3, 1, 'foo']"
+xs.append('bar')    # Add a new element to the end of the list
+print(xs)           # Prints "[3, 1, 'foo', 'bar']"
+x = xs.pop()        # Remove and return the last element of the list
+print(x, xs)        # Prints "bar [3, 1, 'foo']"
